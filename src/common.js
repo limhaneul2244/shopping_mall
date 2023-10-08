eb36a10ab4baa5ccf32b7bf089af347816bf2dca
“limhaneul2244” <hnlim2244@gmail.com>
2023-10-09 00:12:53 +0900

    로컬스토리지에 담긴 값을 가져오는 함수 생성

--- src/common.js
+++ src/common.js
@@ -2,9 +2,11 @@
 
 //로컬스토리지에 담기
 export const setData = (key, value) => {
-  const cartId = localStorage.setItem(key, JSON.stringify(value));
-  console.log('id를 가져왔습니다.', cartId)
+  localStorage.setItem(key, JSON.stringify(value));
 }
 
-
-//로컬스토리지에서 빼기
+//로컬스토리지값 가져오기
+export const getData = (key) => {
+  const value = localStorage.getItem(key);
+  return JSON.parse(value);
+}
